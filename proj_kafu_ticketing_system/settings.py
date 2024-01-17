0a5152e52f2f9ce0ec67856f8678bf690758b47a
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-17 17:01:04 +0300

    setup accounts app

--- proj_kafu_ticketing_system/settings.py
+++ proj_kafu_ticketing_system/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Additional app
+    'accounts', # Handles user accounts
 ]
 
 MIDDLEWARE = [
