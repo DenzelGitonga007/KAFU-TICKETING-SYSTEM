446592063c48115b5ddc9965c8bf3aab77047cec
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-18 15:46:29 +0300

    login and register views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
+
+# Register user
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success("Congratulations! You're logged in.")
+            return redirect('accounts:home') # redirect to home page after successful login/register
+        else:
+            # What should be here?
+            messages.error("Registration failed! Please try again.")
+    else:
+        form = CustomUserCreationForm()
+    context = {'form', form}
+    return render(request, 'accounts/register.html', context)
+
+# Login user
+def login_user(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, "Congratulations! You're logged in")
+            return redirect('accounts:home')
+        else:
+            # What should go here?
+            messages.error(request, "Login failed! Please try again")
+    else:
+        form = CustomAuthenticationForm()
+    context = {'form': form}
+    return render(request, 'accounts/login.html', context)
