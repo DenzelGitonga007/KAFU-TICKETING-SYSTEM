d1acf1d5bd2d34b4a3e17fef5dcccbc78609cac7
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-18 16:22:28 +0300

    admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CustomUser
+
+# Users' model
+class CustomUserAdmin(admin.ModelAdmin):
+    """Manage the users"""
+    list_display = ['username', 'user_type', 'is_superuser'] # fields to display
+    search_fields = ['username', 'user_type'] # fields to search
+
+    def __str__(self):
+        """Stringify the data"""
+        return "{}".format(self.username)
+    
+# Register the admin models
+admin.site.register(CustomUser, CustomUserAdmin)
