ef21e9eef2027dced33e0ae1570c822f509f58bc
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-18 15:50:57 +0300

    templates

--- proj_kafu_ticketing_system/settings.py
+++ proj_kafu_ticketing_system/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'proj_kafu_ticketing_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
