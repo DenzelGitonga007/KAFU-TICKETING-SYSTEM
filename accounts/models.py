924e50381e3d4ec9b74534de5528337ec5cd0e3d
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-18 14:36:36 +0300

    CustomUser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser # Reference the default user model
+
 
 # Create your models here.
+# The customized user model
+class CustomUser(AbstractUser):
+    """The custom user model, from the default user model"""
+    USER_TYPE_CHOICES = [
+        ('client', 'Client'),
+        ('admin', 'Admin'),
+        ('support_staff', 'Support_staff')
+    ] # represent the type of users-- client, is db name, Client, is name on front-end
+
+    user_type = models.CharField(max_length=15, choices=USER_TYPE_CHOICES, default='client') # the user type field, referencing the USER_TYPE_CHOICES
+    # Other fields to be added here
+
+    def __str__(self):
+        """Stringify the values"""
+        return self.username
