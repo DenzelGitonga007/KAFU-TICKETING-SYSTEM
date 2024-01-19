cd0b0cb3576143da3903138c0133b2893189740c
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-19 12:20:11 +0300

    tickets admin site

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Ticket
+
+# Manage the Ticket model
+class TicketAdmin(admin.ModelAdmin):
+    """Manage the ticket details"""
+    list_display = ['ticket_number', 'user']
+    search_fields = ['ticket_number', 'user']
+
+# Register the admin model
+admin.site.register(Ticket, TicketAdmin)
