591351d8a5ea8b6aef50dfcf2f27a5a1a8c97169
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-19 12:31:55 +0300

    upload path for tickets

--- tickets/models.py
+++ tickets/models.py
@@ -9,14 +9,20 @@ def generate_ticket_number():
     """Generate the ticket number"""
     return str(uuid.uuid4()) # eg '550e8400-e29b-41d4-a716-446655440000'
 
+# Upload file
+def upload_path(instance, filename):
+    """Where to upload the file"""
+    return "ticket_attachment/{}/{}/{}".format(generate_ticket_number(), (instance.user.username), filename) # ticket_attachement/ticket_number/username/filename
+
 # Create your models here.
+
 # Ticket model
 class Ticket(models.Model):
     """The ticket details"""
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     ticket_number = models.CharField(max_length=255, unique=True, default=generate_ticket_number, editable=False) # should be assigned by default
     description = models.TextField()
-    attachment = models.FileField(upload_to='ticket_attachement/') 
+    attachment = models.FileField(upload_to=upload_path) 
     # upload to ticket_attachement/username
     # also be able to upload both voice note, and screenshots at the same time/at a go
     created_at = models.DateTimeField(auto_now_add=True)
