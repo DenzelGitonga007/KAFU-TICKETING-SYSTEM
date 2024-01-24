18b722a76294faa70f8aa714f346c0ee11337047
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-24 17:25:50 +0300

    admin_panel models

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from clients.models import Issue # import the Issues model
+from accounts.models import CustomUser # get the user
 
 # Create your models here.
+class Assignment(models.Model):
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE) # get the issue in particular
+    assigned_to = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="assignments") # get the user assigned to
+    assigned_at = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Assignment {}".format(self.id)
