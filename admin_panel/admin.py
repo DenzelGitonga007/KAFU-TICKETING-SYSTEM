8cd6fbe6f93bf2960dc3e28c4f15b8e49509075c
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-25 10:28:28 +0300

    clients admin

--- admin_panel/admin.py
+++ admin_panel/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Assignment
+
+# Assignment admin
+class AssignmentAdmin(admin.ModelAdmin):
+    """Manage the assignments model"""
+    list_display = ["issue", "assigned_to", "is_completed", "assigned_at"]
+    search_fields = ["issue", "assigned_to", "is_completed"]
+
+
+admin.site.register(Assignment, AssignmentAdmin)
