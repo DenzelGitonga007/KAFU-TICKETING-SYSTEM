4e8078863bec9867aee23ae54a2ff929d47b786f
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-25 11:56:59 +0300

    admin_panel view-- assign issues

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages # for dialog messages
+from django.contrib.auth.decorators import user_passes_test # for logged in admin users only
+from .models import Assignment # the assignement model
+from accounts.models import CustomUser # for the user
+from clients.models import Issue # for all issues
+from .forms import AssignmentForm # the customized assignment form
+from django.conf import settings # to get the settings, sender email if needed
+from django.core.mail import send_mail # to send the mail
+
 
 # Create your views here.
+def is_superuser(user):
+    """Test whether user is admin, and is admin"""
+    return user.is_authenticated and user.is_superuser
+
+
+@user_passes_test(is_superuser ,login_url='accounts:login') # admin must be logged in
+# Assignment model
+def admin_panel(request):
+    # Retrieve all issues
+    issues = Issue.objects.all()
+
+    # Retrieve the support staff
+    support_staff = CustomUser.objects.filter(user_type="support_staff")
+
+    # Retrieve all assignments
+    assignments = Assignment.objects.select_related('issues', 'assigned_to').all()
+
+
+    # Assign issues
+    if request.method == "POST":
+        form = AssignmentForm(request.POST)
+        if form.is_valid():
+            assignment_instances = form.save() # the variable for the form instances
+            
+            # Send email to the support staff that a new assignment was done
+            subject = "A new assignment issue {}".format(assignment_instances.issue.ticket_number) # obtain the ticket number of the issue assigned
+            message = "Please log in and find more details about the issue"
+            admin_email = request.user.email # the email of the logged in user/admin
+            support_staff_email = assignment_instances.assigned_to.email # get the email of the support staff
+            # Send the email now
+            send_mail(subject, message, admin_email, [support_staff_email], fail_silently=False)
+            
+            # display success that the assigment was done
+            messages.success(request, "Issue assigned to {} successfully".format(assignment_instances.assigned_to.username))
+            # return to home page for admin
+            return redirect('accounts:home')
+        else:
+            messages.error(request, "Oops! Something went wrong, please try again later")
+    else:
+        form = AssignmentForm()
+    context = {
+        "form": form,
+        "issues": issues,
+        "support_staff": support_staff,
+        "assignments": assignments,
+        }
+    return render(request, "admin_panel/home.html", context)
+
