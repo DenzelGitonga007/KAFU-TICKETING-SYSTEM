840b998c3e206c4e219c12aae7680478229e93b4
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-02-02 12:42:17 +0300

    staff updates, and admin is notified

--- tasks/views.py
+++ tasks/views.py
@@ -32,18 +32,31 @@ def update_tasks(request, task_id):
     if request.method == "POST":
         form = UpdateTaskForm(request.POST, instance=task)
         if form.is_valid():
-            form.save()
-            # send the email
-            subject = "Task update from {}".format(staff.username)
-            message = "Please log in and affirm that task {} is ready to be marked complete".format(task.issue.ticket_number)
-            system_email = settings.EMAIL_HOST_USER # sender email
-            admin_email = settings.EMAIL_HOST_USER # recepient email
+            # save the form and check the status
+            updated_task = form.save()
+            if updated_task.progress_status == "completed": # Notify the admin if task is marked completed by the staff
+                subject = "Task Completed by {}".format(staff.username)
+                message = 'Task {} has been marked as complete by {}. Please review and approve.'.format(updated_task.issue.ticket_number, staff.username)
+                system_email = settings.EMAIL_HOST_USER # sender email
+                admin_email = settings.EMAIL_HOST_USER # recepient email
+                try:
+                    send_mail(subject, message, system_email, [admin_email], fail_silently=False)
+                    messages.success(request, "Task marked completed, and admin has been notified")
+                except Exception as e:
+                    messages.error(request, "Failed to send notification to the admin: {}".format(str(e)))
+            else:
+                messages.success(request, "Task updated successfully.")
+                # send the email
+                subject = "Task update from {}".format(staff.username)
+                message = "Please log in and affirm that task {} is ready to be marked complete".format(task.issue.ticket_number)
+                system_email = settings.EMAIL_HOST_USER # sender email
+                admin_email = settings.EMAIL_HOST_USER # recepient email
 
-            try:
-                send_mail(subject, message, system_email, [admin_email], fail_silently=False)
+                try:
+                    send_mail(subject, message, system_email, [admin_email], fail_silently=False)
 
-            except Exception as e:
-                messages.error(request, "Failed to send the email notification {} to the admin".format(str(e)))
+                except Exception as e:
+                    messages.error(request, "Failed to send the email notification {} to the admin".format(str(e)))
 
             messages.success(request, "Task updated successfully")
             return redirect("accounts:home") # return to the home page
