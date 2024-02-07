8b7e4f495cd2b8893946af09adcf944b6b8b462b
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-02-07 12:39:06 +0300

    sending email to suppor staff on assignment

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -46,7 +46,7 @@ def assign_view(request):
             assignment_instance = form.save() # the variable for the form instances
             
             # Send email to the support staff that a new assignment was done
-            subject = "A new assignment issue {}".format(assignment_instance.issue.ticket_number) # obtain the ticket number of the issue assigned
+            subject = "You have a new assignment issue {}".format(assignment_instance.issue.ticket_number) # obtain the ticket number of the issue assigned
             message = "Please log in and find more details about the issue"
             admin_email = request.user.email # the email of the logged in user/admin
             support_staff_email = assignment_instance.assigned_to.email # get the email of the support staff
